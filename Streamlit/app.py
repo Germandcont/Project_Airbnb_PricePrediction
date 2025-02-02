2bc3bab1354f57977ab53a1098c8db508a05dcea
Germán Domínguez <gdominguezcon@gmail.com>
2025-02-02 17:56:45 +0100

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -23,7 +23,7 @@ st.set_page_config(
 
 
 
-df_cleaned = pd.read_csv('Data/df_model.csv')
+df_cleaned = pd.read_csv('Data/df_clean.csv')
 df_cleaned.columns = df_cleaned.columns.str.upper()
 
 df_model = pd.read_csv('Data/df_model.csv')
